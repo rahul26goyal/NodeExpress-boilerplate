9fadc7b60f6f421482750fba3f600613b421f083
rahul26goyal <goyal.1234rahul@gmail.com>
2017-11-21 12:02:53 +0530

    making app.js standalonef file

--- app.js
+++ app.js
@@ -7,42 +7,47 @@ const bodyParser = require('body-parser');
 
 const Routes = require('./routes/routes')
 
-const app = express();
+function webapp() {
+  const app = express();
 
-//app setup
-app.set('host', process.env.QUBOLE_LOGINHOST || '0.0.0.0');
-app.set('port', normalizePort(process.env.PORT || process.env.QUBOLE_LOGINHPOST || '3000'));
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+  //app setup
+  app.set('host', process.env.QUBOLE_LOGINHOST || '0.0.0.0');
+  app.set('port', normalizePort(process.env.PORT || process.env.QUBOLE_LOGINHPOST || '3000'));
+  app.set('views', path.join(__dirname, 'views'));
+  app.set('view engine', 'pug');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+  // uncomment after placing your favicon in /public
+  //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+  app.use(logger('dev'));
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, 'public')));
 
-//console.log("Routes:::", Routes)
-//app.get('/', )
-let routes = new Routes(app, {})
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  let err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
+  //console.log("Routes:::", Routes)
+  //app.get('/', )
+  let routes = new Routes(app, {})
+  // catch 404 and forward to error handler
+  app.use(function(req, res, next) {
+    let err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+  });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  // error handler
+  app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  });
+
+  //returnt the app object
+  return app;
+}
 
 /**
  * Normalize a port into a number, string, or false.
@@ -64,5 +69,4 @@ function normalizePort(val) {
   return false;
 }
 
-
-module.exports = app;
+module.exports = webapp;
